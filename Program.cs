7547ef6727a5712038a41ce7eb09e6bc11227397
javad <ghaed1993@gmail.com>
2021-01-29 05:29:07 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+namespace javad { 
+namespace namespace2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+  
+        }
+        
+    }
+}
+}
+
+namespace namespace1
+{
+    namespace namespace2
+    {
+
+    }
+}
